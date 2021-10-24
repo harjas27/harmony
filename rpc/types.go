5dd3e8c29b0593c9a15931150012574a976842f0
harjas <harjasbathla@gmail.com>
2021-10-24 14:40:52 +0530

    sync with upstream

--- rpc/types.go
+++ rpc/types.go
@@ -1,313 +1,199 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package rpc
 
 import (
-	"bytes"
-	"encoding/hex"
-	"encoding/json"
+	"context"
 	"fmt"
-	"math/big"
-	"strconv"
+	"github.com/goccy/go-json"
+	"math"
 	"strings"
-	"time"
-
-	internal_common "github.com/harmony-one/harmony/internal/common"
-	"github.com/pkg/errors"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/harmony-one/harmony/block"
-	"github.com/harmony-one/harmony/core/types"
-	"github.com/harmony-one/harmony/internal/utils"
-	"github.com/harmony-one/harmony/numeric"
-	"github.com/harmony-one/harmony/shard"
-	jsoniter "github.com/json-iterator/go"
 )
 
-// CallArgs represents the arguments for a call.
-type CallArgs struct {
-	From     *common.Address `json:"from"`
-	To       *common.Address `json:"to"`
-	Gas      *hexutil.Uint64 `json:"gas"`
-	GasPrice *hexutil.Big    `json:"gasPrice"`
-	Value    *hexutil.Big    `json:"value"`
-	Data     *hexutil.Bytes  `json:"data"`
+// API describes the set of methods offered over the RPC interface
+type API struct {
+	Namespace string      // namespace under which the rpc methods of Service are exposed
+	Version   string      // api version for DApp's
+	Service   interface{} // receiver instance which holds the methods
+	Public    bool        // indication if the methods must be considered safe for public use
 }
 
-// ToMessage converts CallArgs to the Message type used by the core evm
-// Adapted from go-ethereum/internal/ethapi/api.go
-func (args *CallArgs) ToMessage(globalGasCap *big.Int) types.Message {
-	// Set sender address or use zero address if none specified.
-	var addr common.Address
-	if args.From != nil {
-		addr = *args.From
-	}
-
-	// Set default gas & gas price if none were set
-	gas := uint64(math.MaxUint64 / 2)
-	if args.Gas != nil {
-		gas = uint64(*args.Gas)
-	}
-	if globalGasCap != nil && globalGasCap.Uint64() < gas {
-		utils.Logger().Warn().
-			Uint64("requested", gas).
-			Uint64("cap", globalGasCap.Uint64()).
-			Msg("Caller gas above allowance, capping")
-		gas = globalGasCap.Uint64()
-	}
-	gasPrice := new(big.Int)
-	if args.GasPrice != nil {
-		gasPrice = args.GasPrice.ToInt()
-	}
-
-	value := new(big.Int)
-	if args.Value != nil {
-		value = args.Value.ToInt()
-	}
-
-	var data []byte
-	if args.Data != nil {
-		data = []byte(*args.Data)
-	}
-
-	msg := types.NewMessage(addr, args.To, 0, value, gas, gasPrice, data, false)
-	return msg
+// Error wraps RPC errors, which contain an error code in addition to the message.
+type Error interface {
+	Error() string  // returns the message
+	ErrorCode() int // returns the code
 }
 
-// StakingNetworkInfo returns global staking info.
-type StakingNetworkInfo struct {
-	TotalSupply       numeric.Dec `json:"total-supply"`
-	CirculatingSupply numeric.Dec `json:"circulating-supply"`
-	EpochLastBlock    uint64      `json:"epoch-last-block"`
-	TotalStaking      *big.Int    `json:"total-staking"`
-	MedianRawStake    numeric.Dec `json:"median-raw-stake"`
+// ServerCodec implements reading, parsing and writing RPC messages for the server side of
+// a RPC session. Implementations must be go-routine safe since the codec can be called in
+// multiple go-routines concurrently.
+type ServerCodec interface {
+	readBatch() (msgs []*jsonrpcMessage, isBatch bool, err error)
+	close()
+	jsonWriter
 }
 
-// Delegation represents a particular delegation to a validator
-type Delegation struct {
-	ValidatorAddress string         `json:"validator_address"`
-	DelegatorAddress string         `json:"delegator_address"`
-	Amount           *big.Int       `json:"amount"`
-	Reward           *big.Int       `json:"reward"`
-	Undelegations    []Undelegation `json:"Undelegations"`
+// jsonWriter can write JSON messages to its underlying connection.
+// Implementations must be safe for concurrent use.
+type jsonWriter interface {
+	writeJSON(context.Context, interface{}) error
+	// Closed returns a channel which is closed when the connection is closed.
+	closed() <-chan interface{}
+	// RemoteAddr returns the peer address of the connection.
+	remoteAddr() string
 }
 
-// Undelegation represents one undelegation entry
-type Undelegation struct {
-	Amount *big.Int
-	Epoch  *big.Int
-}
-
-// StructuredResponse type of RPCs
-type StructuredResponse = map[string]interface{}
-
-// NewStructuredResponse creates a structured response from the given input
-func NewStructuredResponse(input interface{}) (StructuredResponse, error) {
-	var objMap StructuredResponse
-	var jsonIter = jsoniter.ConfigCompatibleWithStandardLibrary
-	dat, err := jsonIter.Marshal(input)
-	if err != nil {
-		return nil, err
-	}
-	d := jsonIter.NewDecoder(bytes.NewReader(dat))
-	d.UseNumber()
-	err = d.Decode(&objMap)
-	if err != nil {
-		return nil, err
-	}
-	return objMap, nil
-}
-
-// BlockNumber ..
-type BlockNumber rpc.BlockNumber
+type BlockNumber int64
 
 const (
-	// LatestBlockNumber is the alias to rpc latest block number
-	LatestBlockNumber = BlockNumber(rpc.LatestBlockNumber)
-
-	// PendingBlockNumber is the alias to rpc pending block number
-	PendingBlockNumber = BlockNumber(rpc.PendingBlockNumber)
+	PendingBlockNumber  = BlockNumber(-2)
+	LatestBlockNumber   = BlockNumber(-1)
+	EarliestBlockNumber = BlockNumber(0)
 )
 
-// UnmarshalJSON converts a hex string or integer to a block number
+// UnmarshalJSON parses the given JSON fragment into a BlockNumber. It supports:
+// - "latest", "earliest" or "pending" as string arguments
+// - the block number
+// Returned errors:
+// - an invalid block number error when the given argument isn't a known strings
+// - an out of range error when the given block number is either too little or too large
 func (bn *BlockNumber) UnmarshalJSON(data []byte) error {
-	baseBn := rpc.BlockNumber(0)
-	baseErr := baseBn.UnmarshalJSON(data)
-	if baseErr != nil {
-		input := strings.TrimSpace(string(data))
-		if len(input) >= 2 && input[0] == '"' && input[len(input)-1] == '"' {
-			input = input[1 : len(input)-1]
-		}
-		input = strings.TrimPrefix(input, "0x")
-		num, err := strconv.ParseInt(input, 10, 64)
-		if err != nil {
-			return err
-		}
-		*bn = BlockNumber(num)
-		return nil
-	}
-	*bn = BlockNumber(baseBn)
-	return nil
-}
-
-// Int64 ..
-func (bn BlockNumber) Int64() int64 {
-	return (int64)(bn)
-}
-
-// EthBlockNumber ..
-func (bn BlockNumber) EthBlockNumber() rpc.BlockNumber {
-	return (rpc.BlockNumber)(bn)
-}
-
-// TransactionIndex ..
-type TransactionIndex uint64
-
-// UnmarshalJSON converts a hex string or integer to a Transaction index
-func (i *TransactionIndex) UnmarshalJSON(data []byte) (err error) {
 	input := strings.TrimSpace(string(data))
 	if len(input) >= 2 && input[0] == '"' && input[len(input)-1] == '"' {
 		input = input[1 : len(input)-1]
 	}
 
-	var num int64
-	if strings.HasPrefix(input, "0x") {
-		num, err = strconv.ParseInt(strings.TrimPrefix(input, "0x"), 16, 64)
-	} else {
-		num, err = strconv.ParseInt(input, 10, 64)
+	switch input {
+	case "earliest":
+		*bn = EarliestBlockNumber
+		return nil
+	case "latest":
+		*bn = LatestBlockNumber
+		return nil
+	case "pending":
+		*bn = PendingBlockNumber
+		return nil
 	}
+
+	blckNum, err := hexutil.DecodeUint64(input)
 	if err != nil {
 		return err
 	}
-
-	*i = TransactionIndex(num)
+	if blckNum > math.MaxInt64 {
+		return fmt.Errorf("block number larger than int64")
+	}
+	*bn = BlockNumber(blckNum)
 	return nil
 }
 
-// TxHistoryArgs is struct to include optional transaction formatting params.
-type TxHistoryArgs struct {
-	Address   string `json:"address"`
-	PageIndex uint32 `json:"pageIndex"`
-	PageSize  uint32 `json:"pageSize"`
-	FullTx    bool   `json:"fullTx"`
-	TxType    string `json:"txType"`
-	Order     string `json:"order"`
+func (bn BlockNumber) Int64() int64 {
+	return (int64)(bn)
 }
 
-// UnmarshalFromInterface ..
-func (ta *TxHistoryArgs) UnmarshalFromInterface(blockArgs interface{}) error {
-	var args TxHistoryArgs
-	dat, err := json.Marshal(blockArgs)
+type BlockNumberOrHash struct {
+	BlockNumber      *BlockNumber `json:"blockNumber,omitempty"`
+	BlockHash        *common.Hash `json:"blockHash,omitempty"`
+	RequireCanonical bool         `json:"requireCanonical,omitempty"`
+}
+
+func (bnh *BlockNumberOrHash) UnmarshalJSON(data []byte) error {
+	type erased BlockNumberOrHash
+	e := erased{}
+	err := json.Unmarshal(data, &e)
+	if err == nil {
+		if e.BlockNumber != nil && e.BlockHash != nil {
+			return fmt.Errorf("cannot specify both BlockHash and BlockNumber, choose one or the other")
+		}
+		bnh.BlockNumber = e.BlockNumber
+		bnh.BlockHash = e.BlockHash
+		bnh.RequireCanonical = e.RequireCanonical
+		return nil
+	}
+	var input string
+	err = json.Unmarshal(data, &input)
 	if err != nil {
 		return err
 	}
-	if err := json.Unmarshal(dat, &args); err != nil {
-		return err
-	}
-	*ta = args
-	return nil
-}
-
-// HeaderInformation represents the latest consensus information
-type HeaderInformation struct {
-	BlockHash        common.Hash       `json:"blockHash"`
-	BlockNumber      uint64            `json:"blockNumber"`
-	ShardID          uint32            `json:"shardID"`
-	Leader           string            `json:"leader"`
-	ViewID           uint64            `json:"viewID"`
-	Epoch            uint64            `json:"epoch"`
-	Timestamp        string            `json:"timestamp"`
-	UnixTime         uint64            `json:"unixtime"`
-	LastCommitSig    string            `json:"lastCommitSig"`
-	LastCommitBitmap string            `json:"lastCommitBitmap"`
-	VRF              string            `json:"vrf"`
-	VRFProof         string            `json:"vrfProof"`
-	CrossLinks       *types.CrossLinks `json:"crossLinks,omitempty"`
-}
-
-// NewHeaderInformation returns the header information that will serialize to the RPC representation.
-func NewHeaderInformation(header *block.Header, leader string) *HeaderInformation {
-	if header == nil {
+	switch input {
+	case "earliest":
+		bn := EarliestBlockNumber
+		bnh.BlockNumber = &bn
 		return nil
-	}
-
-	vrfAndProof := header.Vrf()
-	vrf := common.Hash{}
-	vrfProof := []byte{}
-	if len(vrfAndProof) == 32+96 {
-		copy(vrf[:], vrfAndProof[:32])
-		vrfProof = vrfAndProof[32:]
-	}
-	result := &HeaderInformation{
-		BlockHash:        header.Hash(),
-		BlockNumber:      header.Number().Uint64(),
-		ShardID:          header.ShardID(),
-		Leader:           leader,
-		ViewID:           header.ViewID().Uint64(),
-		Epoch:            header.Epoch().Uint64(),
-		UnixTime:         header.Time().Uint64(),
-		Timestamp:        time.Unix(header.Time().Int64(), 0).UTC().String(),
-		LastCommitBitmap: hex.EncodeToString(header.LastCommitBitmap()),
-		VRF:              hex.EncodeToString(vrf[:]),
-		VRFProof:         hex.EncodeToString(vrfProof),
-	}
-
-	sig := header.LastCommitSignature()
-	result.LastCommitSig = hex.EncodeToString(sig[:])
-
-	if header.ShardID() == shard.BeaconChainShardID {
-		decodedCrossLinks := &types.CrossLinks{}
-		err := rlp.DecodeBytes(header.CrossLinks(), decodedCrossLinks)
-		if err != nil {
-			result.CrossLinks = &types.CrossLinks{}
-		} else {
-			result.CrossLinks = decodedCrossLinks
-		}
-	}
-
-	return result
-}
-
-// AddressOrList represents an address or a list of addresses
-type AddressOrList struct {
-	Address     *common.Address
-	AddressList []common.Address
-}
-
-// UnmarshalJSON defines the input parsing of AddressOrList
-func (aol *AddressOrList) UnmarshalJSON(data []byte) (err error) {
-	var itf interface{}
-	if err := json.Unmarshal(data, &itf); err != nil {
-		return err
-	}
-	switch d := itf.(type) {
-	case string: // Single address
-		addr, err := internal_common.ParseAddr(d)
-		if err != nil {
-			return err
-		}
-		aol.Address = &addr
+	case "latest":
+		bn := LatestBlockNumber
+		bnh.BlockNumber = &bn
 		return nil
-
-	case []interface{}: // Address array
-		var addrs []common.Address
-		for _, addrItf := range d {
-			addrStr, ok := addrItf.(string)
-			if !ok {
-				return errors.New("not invalid address array")
-			}
-			addr, err := internal_common.ParseAddr(addrStr)
-			if err != nil {
-				return fmt.Errorf("invalid address: %v", addrStr)
-			}
-			addrs = append(addrs, addr)
-		}
-		aol.AddressList = addrs
+	case "pending":
+		bn := PendingBlockNumber
+		bnh.BlockNumber = &bn
 		return nil
-
 	default:
-		return errors.New("must provide one address or address list")
+		if len(input) == 66 {
+			hash := common.Hash{}
+			err := hash.UnmarshalText([]byte(input))
+			if err != nil {
+				return err
+			}
+			bnh.BlockHash = &hash
+			return nil
+		} else {
+			blckNum, err := hexutil.DecodeUint64(input)
+			if err != nil {
+				return err
+			}
+			if blckNum > math.MaxInt64 {
+				return fmt.Errorf("blocknumber too high")
+			}
+			bn := BlockNumber(blckNum)
+			bnh.BlockNumber = &bn
+			return nil
+		}
+	}
+}
+
+func (bnh *BlockNumberOrHash) Number() (BlockNumber, bool) {
+	if bnh.BlockNumber != nil {
+		return *bnh.BlockNumber, true
+	}
+	return BlockNumber(0), false
+}
+
+func (bnh *BlockNumberOrHash) Hash() (common.Hash, bool) {
+	if bnh.BlockHash != nil {
+		return *bnh.BlockHash, true
+	}
+	return common.Hash{}, false
+}
+
+func BlockNumberOrHashWithNumber(blockNr BlockNumber) BlockNumberOrHash {
+	return BlockNumberOrHash{
+		BlockNumber:      &blockNr,
+		BlockHash:        nil,
+		RequireCanonical: false,
+	}
+}
+
+func BlockNumberOrHashWithHash(hash common.Hash, canonical bool) BlockNumberOrHash {
+	return BlockNumberOrHash{
+		BlockNumber:      nil,
+		BlockHash:        &hash,
+		RequireCanonical: canonical,
 	}
 }
